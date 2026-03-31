2ea1d632565ab7b38e11ac0a742d44907d541612
Darshan <darshan.challani18@gmail.com>
2026-03-31 14:49:23 +0530

    Fix: base path and router for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nemra-travels/', // This must match your repo name exactly
   plugins: [react()],
+  base: '/nemra-travels/', // This must match your GitHub repository name exactly
 })
